dedbc44a85b72a8688824b52c973fa7c1cec4097
Salvatore Calo <dafder45678@gmail.com>
2026-03-07 12:49:50 +0100

    fix: now show real number of active and total modules and not hardcoded values

--- lib/features/modules_page/components/symbol_legend/symbol_legend.dart
+++ lib/features/modules_page/components/symbol_legend/symbol_legend.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:isaac_app/features/modules_page/components/module_card/provider/number_of_active_modules_provider/number_of_active_module_provider.dart';
+import 'package:isaac_app/features/modules_page/components/module_card/provider/number_of_modules_provider/number_of_module_provider.dart';
 
-class SymbolLegend extends StatelessWidget {
+class SymbolLegend extends ConsumerWidget {
   const SymbolLegend({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final numberOfActiveModule = ref.watch(numberOfActiveModulesProvider);
+    final totalNumberOfModules = ref.watch(numberOfModulesProvider);
     return Column(
       spacing: 15,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("Active modules: 5 su 60"),
+        Text("Active modules: $numberOfActiveModule su $totalNumberOfModules"),
         SizedBox(
           width: MediaQuery.of(context).size.width,
           child: ExpansionTile(

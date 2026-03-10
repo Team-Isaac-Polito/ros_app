c5aaa4f61530d3efd524c8990de5a24498a32ecf
Salvatore Calo <dafder45678@gmail.com>
2026-03-10 21:00:11 +0100

    fix: changed refresh_socket_connection to consumer widget
    Signed-off-by: Salvatore Calo <dafder45678@gmail.com>

--- lib/features/main_page/components/refresh_socket_connection/refresh_socket_connection.dart
+++ lib/features/main_page/components/refresh_socket_connection/refresh_socket_connection.dart
@@ -2,18 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:isaac_app/features/main_page/data/ros_bridge_provider/ros_bridge_provider.dart';
 
-class RefreshSocketConnection extends ConsumerStatefulWidget {
+class RefreshSocketConnection extends ConsumerWidget {
   const RefreshSocketConnection({super.key});
-
   @override
-  ConsumerState<RefreshSocketConnection> createState() =>
-      _RefreshSocketConnectionState();
-}
-
-class _RefreshSocketConnectionState
-    extends ConsumerState<RefreshSocketConnection> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return IconButton(
       icon: Icon(Icons.refresh),
       onPressed: () {
